e3a6bc2941cdeaafbbc22bbc41e70e4bfedf87fb
Hilary Palevsky <palevsky@bc.edu>
2022-08-16 10:39:34 -0400

    Updated wfp_lag.m to loop over all years
    Output saved in lagyr1to7.mat since this is computationally intensive; had to run in batches because had to special case most years to avoid glitches in tau calculation function (line 89)

--- wfp_lag.m
+++ wfp_lag.m
@@ -3,17 +3,8 @@
 % Runs after 1st code block in wfp_analysis.m, which reads in all years of
 % WFP O2 data and passes through load_HYPM_DOSTA_fun
 
-%Make first attempt to do this just for Year 1 wfp data (now expanding to year 2)
-testyr = 2;
-
-%% Roo's version of correction, example in ws_O2tau.m for glider
-% load('Tau4330_lookup.mat','T4330');
-% [tau] = optode_tau(wfp{testyr}.time_dosta_mat, wfp{testyr}.pressure_dosta, wfp{testyr}.temperature_dosta, T4330);
-% Note that there is an error that leads to all NaNs in this calc - haven't
-% gone further down the path of troubleshooting, since this approach
-% doesn't empirically determine the boundary layer thickness (uses values
-% from Bittig & Kortzinger, 2017, section 3 - Figure 1 and eqns 1 & 2),
-% which may not be applicable to WFP since determined for floats & gliders
+% Loop over all years
+for yr = 1:2
 
 %% Gordon et al. 2020 version of correction
 % Format WFP output into matrices that match the required argument formats
@@ -21,48 +12,62 @@ testyr = 2;
 
 tol = 50; %only use profiles with at least 50 points
 
-numprofiles = max(wfp{testyr}.profile_index);
-    wgg.mtime = NaN(numprofiles,2000);
-    wgg.pres = NaN(numprofiles,2000);
-    wgg.doxy = NaN(numprofiles,2000);
-    wgg.temp = NaN(numprofiles,2000);
-    wgg.pdens = NaN(numprofiles,2000);
-    wgg.updown = NaN(numprofiles,1);
-    
+numprofiles = max(wfp{yr}.profile_index);
+    wgg{yr}.mtime = NaN(numprofiles,2000);
+    wgg{yr}.pres = NaN(numprofiles,2000);
+    wgg{yr}.doxy = NaN(numprofiles,2000);
+    wgg{yr}.temp = NaN(numprofiles,2000);
+    wgg{yr}.pdens = NaN(numprofiles,2000);
+    wgg{yr}.updown = NaN(numprofiles,1);
 for i = 1:numprofiles
-    indp = find(wfp{testyr}.profile_index == i);
+    indp = find(wfp{yr}.profile_index == i);
     if length(indp) > tol
-        wgg.mtime(i,1:length(indp)) = wfp{testyr}.time_dosta_mat(indp);
-        wgg.pres(i,1:length(indp)) = wfp{testyr}.pressure_dosta(indp);
-        wgg.doxy(i,1:length(indp)) = wfp{testyr}.oxygen(indp);
-        wgg.temp(i,1:length(indp)) = wfp{testyr}.temperature_dosta(indp);
-        wgg.pdens(i,1:length(indp)) = wfp{testyr}.pdens(indp);
-        wgg.updown(i) = wfp{testyr}.updown_index(indp(1+tol));
+        wgg{yr}.mtime(i,1:length(indp)) = wfp{yr}.time_dosta_mat(indp);
+        wgg{yr}.pres(i,1:length(indp)) = wfp{yr}.pressure_dosta(indp);
+        wgg{yr}.doxy(i,1:length(indp)) = wfp{yr}.oxygen(indp);
+        wgg{yr}.temp(i,1:length(indp)) = wfp{yr}.temperature_dosta(indp);
+        wgg{yr}.pdens(i,1:length(indp)) = wfp{yr}.pdens(indp);
+        wgg{yr}.updown(i) = wfp{yr}.updown_index(indp(1+tol));
     end
-    indzero = find(wgg.doxy(i,:) == 0);
-    wgg.doxy(i,indzero) = NaN;
+    indzero = find(wgg{yr}.doxy(i,:) == 0);
+    wgg{yr}.doxy(i,indzero) = NaN;
 end
 
 %% Cut out rows and columns of all NaNs
-ind_good = find(~all(isnan(wgg.doxy')) & ~isnan(wgg.updown)' == 1); %includes > tol pts and has up/down index
-    wgg.mtime = wgg.mtime(ind_good,~all(isnan(wgg.doxy)));
-    wgg.pres = wgg.pres(ind_good,~all(isnan(wgg.doxy)));
-    wgg.doxy = wgg.doxy(ind_good,~all(isnan(wgg.doxy)));
-    wgg.temp = wgg.temp(ind_good,~all(isnan(wgg.doxy)));
-    wgg.pdens = wgg.pdens(ind_good,~all(isnan(wgg.doxy)));
-    wgg.updown = wgg.updown(ind_good);
+ind_good = find(~all(isnan(wgg{yr}.doxy')) & ~isnan(wgg{yr}.updown)' == 1); %includes > tol pts and has up/down index
+    wgg{yr}.mtime = wgg{yr}.mtime(ind_good,~all(isnan(wgg{yr}.doxy)));
+    wgg{yr}.pres = wgg{yr}.pres(ind_good,~all(isnan(wgg{yr}.doxy)));
+    wgg{yr}.doxy = wgg{yr}.doxy(ind_good,~all(isnan(wgg{yr}.doxy)));
+    wgg{yr}.temp = wgg{yr}.temp(ind_good,~all(isnan(wgg{yr}.doxy)));
+    wgg{yr}.pdens = wgg{yr}.pdens(ind_good,~all(isnan(wgg{yr}.doxy)));
+    wgg{yr}.updown = wgg{yr}.updown(ind_good);
     
 %% Identify gaps in continuous paired profile sampling
-    ttol = 1; %tolerance for gap between profiles, in days
-tgap = find(diff(wgg.mtime(:,1)) > ttol); %Calculate time between profiles
-pgap = find(abs(diff(wgg.updown)) < 2); %== 2 when switching from -1 to 1
-gaps = union(tgap,pgap);
+    ttol = 2; %tolerance for gap between profiles, in days
+wgg{yr}.tgap = find(diff(wgg{yr}.mtime(:,1)) > ttol); %Calculate time between profiles
+wgg{yr}.pgap = find(abs(diff(wgg{yr}.updown)) < 2); %== 2 when switching from -1 to 1
+wgg{yr}.gaps = union(wgg{yr}.tgap,wgg{yr}.pgap);
     
 %% Calculate tau using Gordon et al. 2020 method
 addpath(genpath('C:\Users\palevsky\Documents\GitHub\optode-response-time'))
 
-%rng = [1:69, 71:112, 120:183, 201:241, 243:273, 276:351, 353:393]; %use full range now that have identified which version to go forward with - hard coded based on gaps output
-rng = [1:89,91:382]; %set for year 2
+if yr == 1
+    rng = [1:69, 71:112, 120:183, 201:241, 243:273, 276:351, 353:393];
+elseif yr == 2
+    rng = [1:89,91:382];
+elseif yr == 3
+    rng = [1:238, 242:388, 392:length(wgg{yr}.updown)];
+elseif yr == 4
+    rng = [1:185, 188:224, 256:268, 293:362, 375:length(wgg{yr}.updown)];
+elseif yr == 6
+    %rng = [1:length(wgg{yr}.updown)]; rng(wgg{yr}.gaps) = NaN; rng(240:279) = NaN; rng = rng(~isnan(rng));
+    %rng = [1:57, 59:93, 102:188, 209:222, 224:272, 286:327];
+    rng = [1:57];
+elseif yr == 7
+    rng = [1:79];
+else
+    rng = [1:length(wgg{yr}.updown)]; rng(wgg{yr}.gaps) = NaN; rng = rng(~isnan(rng));
+end
 tref = 4; %reference temperature for tau
 zlim_depth = [300 2500];
 zlim_dens = [1027.62 1027.92]; %density bounds over which to calculate tau (~+/- 2.5 std from mean in yr1)
@@ -81,7 +86,7 @@ zres = 0.0005;
 
 %Version with temperature term, also in density space
 tic
-[wgg.thicknessd, wgg.tau_Trefd, thickness_constants, wgg.rmsdtd] = calculate_tau_wTemp( wgg.mtime(rng,:), wgg.pdens(rng,:), wgg.doxy(rng,:), wgg.temp(rng,:),...
+[wgg{yr}.thicknessd, wgg{yr}.tau_Trefd, wgg{yr}.thickness_constants, wgg{yr}.rmsdtd] = calculate_tau_wTemp( wgg{yr}.mtime(rng,:), wgg{yr}.pdens(rng,:), wgg{yr}.doxy(rng,:), wgg{yr}.temp(rng,:),...
     'zlim',zlim_dens, 'zres', zres, 'tref', tref);
 toc
 length(rng)
@@ -92,63 +97,66 @@ length(rng)
 % indnotgap = find(diff(rng) == 1);
 %Don't fill gaps because it prevents movmean plotting
 
-figure(100); clf
+figure(100 + yr); clf
     smthval = 30;
 subplot(211)
-plot(wgg.mtime(rng(1:end-1),1), wgg.thicknessd,'k.'); hold on;
-%plot(wgg.mtime(rng(indgap),1), wgg.thicknessd(indgap),'b.','markersize',10); hold on;
-plot(wgg.mtime(rng(1:end-1),1), movmean(wgg.thicknessd,smthval),'r.'); hold on;
-plot(wgg.mtime(rng(1:end-1),1), movmean(wgg.thicknessd,smthval) + movstd(wgg.thicknessd,smthval),'r-'); hold on;
-plot(wgg.mtime(rng(1:end-1),1), movmean(wgg.thicknessd,smthval) - movstd(wgg.thicknessd,smthval),'r-'); hold on;
+plot(wgg{yr}.mtime(rng(1:end-1),1), wgg{yr}.thicknessd,'k.'); hold on;
+%plot(wgg{yr}.mtime(rng(indgap),1), wgg{yr}.thicknessd(indgap),'b.','markersize',10); hold on;
+plot(wgg{yr}.mtime(rng(1:end-1),1), movmean(wgg{yr}.thicknessd,smthval),'r.'); hold on;
+plot(wgg{yr}.mtime(rng(1:end-1),1), movmean(wgg{yr}.thicknessd,smthval) + movstd(wgg{yr}.thicknessd,smthval),'r-'); hold on;
+plot(wgg{yr}.mtime(rng(1:end-1),1), movmean(wgg{yr}.thicknessd,smthval) - movstd(wgg{yr}.thicknessd,smthval),'r-'); hold on;
 datetick('x')
 ylabel('Thickness (\mum)')
 legend('Individual points','30-pt moving mean','+/- 30-pt moving stdev')
-title('OOI Irminger WFP Year 2 lag, Gordon et al. 2020 T-dependent method calc. in density space')
+title(['OOI Irminger WFP Year ' num2str(yr) ' lag, Gordon et al. 2020 T-dependent method calc. in density space'])
 
 
 subplot(212)
-plot(wgg.mtime(rng(1:end-1),1), wgg.tau_Trefd,'k.'); hold on;
-%plot(wgg.mtime(rng(indgap),1), wgg.tau_Trefd(indgap),'b.','markersize',10); hold on;
-plot(wgg.mtime(rng(1:end-1),1), movmean(wgg.tau_Trefd,smthval),'r.'); hold on;
-plot(wgg.mtime(rng(1:end-1),1), movmean(wgg.tau_Trefd,smthval) + movstd(wgg.tau_Trefd,smthval),'r-'); hold on;
-plot(wgg.mtime(rng(1:end-1),1), movmean(wgg.tau_Trefd,smthval) - movstd(wgg.tau_Trefd,smthval),'r-'); hold on;
+plot(wgg{yr}.mtime(rng(1:end-1),1), wgg{yr}.tau_Trefd,'k.'); hold on;
+%plot(wgg{yr}.mtime(rng(indgap),1), wgg{yr}.tau_Trefd(indgap),'b.','markersize',10); hold on;
+plot(wgg{yr}.mtime(rng(1:end-1),1), movmean(wgg{yr}.tau_Trefd,smthval),'r.'); hold on;
+plot(wgg{yr}.mtime(rng(1:end-1),1), movmean(wgg{yr}.tau_Trefd,smthval) + movstd(wgg{yr}.tau_Trefd,smthval),'r-'); hold on;
+plot(wgg{yr}.mtime(rng(1:end-1),1), movmean(wgg{yr}.tau_Trefd,smthval) - movstd(wgg{yr}.tau_Trefd,smthval),'r-'); hold on;
 datetick('x')
 ylabel('\tau (s) at 4^oC')
 legend('Individual points','30-pt moving mean','+/- 30-pt moving stdev')
 
 % Calculate annual stats on datapoints excluding the gaps
-wgg.stats = [nanmean(wgg.thicknessd) nanstd(wgg.thicknessd) length(wgg.thicknessd);...
-    nanmean(wgg.tau_Trefd) nanstd(wgg.tau_Trefd) length(wgg.tau_Trefd)];
-wgg.stats(:,4) = wgg.stats(:,2)./sqrt(wgg.stats(:,3));
+wgg{yr}.stats = [nanmean(wgg{yr}.thicknessd) nanstd(wgg{yr}.thicknessd) length(wgg{yr}.thicknessd);...
+    nanmean(wgg{yr}.tau_Trefd) nanstd(wgg{yr}.tau_Trefd) length(wgg{yr}.tau_Trefd)];
+wgg{yr}.stats(:,4) = wgg{yr}.stats(:,2)./sqrt(wgg{yr}.stats(:,3));
 
 
 %% Apply tau correction using version w/ temperature term & density space
-[m,n] = size(wgg.doxy);
+[m,n] = size(wgg{yr}.doxy);
 
 %Initialize array to hold corrected oxygen data
-wgg.doxy_lagcorr = NaN(size(wgg.doxy)); %with temperature correction (Gordon et al. 2020 version w/ Bittig add on)
-wgg.doxy_lagcorr2 = NaN(size(wgg.doxy)); %Roo's implementation, no temperature correction
+wgg{yr}.doxy_lagcorr = NaN(size(wgg{yr}.doxy)); %with temperature correction (Gordon et al. 2020 version w/ Bittig add on)
+wgg{yr}.doxy_lagcorr2 = NaN(size(wgg{yr}.doxy)); %Roo's implementation, no temperature correction
 
 for i = 1:m
-    ind = ~(isnan(wgg.mtime(i,:)) | isnan(wgg.doxy(i,:)) | isnan(wgg.temp(i,:)));
-    wgg.doxy_lagcorr(i,ind) = correct_oxygen_profile_wTemp(wgg.mtime(i,ind), wgg.doxy(i,ind), wgg.temp(i,ind), nanmean(wgg.thicknessd));
-    wgg.doxy_lagcorr2(i,ind) = aa_deconv(wgg.mtime(i,ind), wgg.doxy(i,ind), nanmean(wgg.tau_Tref));
+    ind = ~(isnan(wgg{yr}.mtime(i,:)) | isnan(wgg{yr}.doxy(i,:)) | isnan(wgg{yr}.temp(i,:)));
+    wgg{yr}.doxy_lagcorr(i,ind) = correct_oxygen_profile_wTemp(wgg{yr}.mtime(i,ind), wgg{yr}.doxy(i,ind), wgg{yr}.temp(i,ind), nanmean(wgg{yr}.thicknessd));
+    wgg{yr}.doxy_lagcorr2(i,ind) = aa_deconv(wgg{yr}.mtime(i,ind), wgg{yr}.doxy(i,ind), nanmean(wgg{yr}.tau_Trefd));
+end
+
 end
 
 %% Plot examples of paired up & down profiles
 smthval = 10;
 M = 4;
 int = 80;
+yr = 6;
 figure(1); clf
 for i = 1:4
     subplot(2,2,i)
-    plot(wgg.doxy(i*int,:), wgg.pres(i*int,:),'k.'); hold on;
-    plot(wgg.doxy(i*int + 1,:), wgg.pres(i*int + 1,:),'.'); hold on;
-    plot(movmean(wgg.doxy_lagcorr(i*int,:),smthval), wgg.pres(i*int,:),'.','color',nicecolor('kww'),'markersize',M); hold on;
-    plot(movmean(wgg.doxy_lagcorr(i*int + 1,:),smthval), wgg.pres(i*int + 1,:),'.','color',nicecolor('rrykkwwww'),'markersize',M); hold on;
+    plot(wgg{yr}.doxy(i*int,:), wgg{yr}.pres(i*int,:),'k.'); hold on;
+    plot(wgg{yr}.doxy(i*int + 1,:), wgg{yr}.pres(i*int + 1,:),'.'); hold on;
+    plot(movmean(wgg{yr}.doxy_lagcorr(i*int,:),smthval), wgg{yr}.pres(i*int,:),'.','color',nicecolor('kww'),'markersize',M); hold on;
+    plot(movmean(wgg{yr}.doxy_lagcorr(i*int + 1,:),smthval), wgg{yr}.pres(i*int + 1,:),'.','color',nicecolor('rrykkwwww'),'markersize',M); hold on;
     xlabel('Oxygen-L2, (\mumol/kg)')
     ylabel('dbar')
-    title(datestr(wgg.mtime(i*int + 1,1),1))
+    title(datestr(wgg{yr}.mtime(i*int + 1,1),1))
     legend('Up','Down','location','northwest')
     axis ij
 end
@@ -172,14 +180,15 @@ figure(2); clf
 % ylabel('RMSD')
 % title('w/ T term, depth aligned')
 %     subplot(224)
-plot(wgg.thickness_constants,wgg.rmsdtd,'.')
+plot(wgg{6}.thickness_constants,wgg{6}.rmsdtd,'.')
 xlabel('thickness (\mum)')
 ylabel('RMSD')
 title('w/ T term, density aligned')
 
 %% Calculate WFP velocity in dbar s-1 to compare with Bittig & Kortzinger 2017
-v = diff(wfp{testyr}.pressure_dosta)./(diff(wfp{testyr}.time_dosta_mat)*(24*60*60));
-figure(3); clf
-histogram(abs(v))
-title({'WFP Yr 1 velocity histogram, median = ' num2str(nanmedian(abs(v)),3)})
-xlabel('|dbar s^{-1}|')
+% v = diff(wfp{testyr}.pressure_dosta)./(diff(wfp{testyr}.time_dosta_mat)*(24*60*60));
+% figure(3); clf
+% histogram(abs(v))
+% title({'WFP Yr 1 velocity histogram, median = ' num2str(nanmedian(abs(v)),3)})
+% xlabel('|dbar s^{-1}|')
+
